fd15554b668b08e2e470124416c6656f9686b89f
Shohidul Arifin <shohidularifin@gmail.com>
2023-06-13 13:56:59 -0700

    create-payment-intent route added

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const jwt = require('jsonwebtoken');
 const app = express();
 const cors = require('cors');
+const stripe = require('stripe')(process.env.PAYMENT_SECRET_KEY)
 
 require('dotenv').config();
 const port = process.env.PORT || 5000;
@@ -88,6 +89,12 @@ async function run() {
       res.send(result);
 
 
+    })
+    // Add a class
+    app.post('/classes',verifyJWT, async(req,res)=>{
+      const newClass = req.body;
+      const result = await classCollection.insertOne(newClass);
+      res.send(result);
     })
 
     app.get('/popularclasses', async(req,res)=>{
@@ -199,6 +206,21 @@ async function run() {
       const query ={_id:new ObjectId(id)};
       const result = await cartsCollection.deleteOne(query);
       res.send(result);
+    })
+
+     // payment intent
+     app.post('/create-payment-intent',verifyJWT, async (req, res) => {
+      const { price } = req.body;
+      const amount = parseInt(price * 100);
+      const paymentIntent = await stripe.paymentIntents.create({
+        amount: amount,
+        currency: 'usd',
+        payment_method_types: ['card']
+      });
+
+      res.send({
+        clientSecret: paymentIntent.client_secret
+      })
     })
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
